828fbc3517c1bd43ce5c3ef804dfad13770fef5c
Icedragal <icedragal@gmail.com>
2022-12-27 15:26:22 +0300

    Доработка

--- src/main/java/me/zhuravlevegor/mavenhomework/controllers/FirstController.java
+++ src/main/java/me/zhuravlevegor/mavenhomework/controllers/FirstController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstController {
-    @GetMapping
+    @GetMapping("/")
     public String print(){
         return "Приложение запущено";
     }
