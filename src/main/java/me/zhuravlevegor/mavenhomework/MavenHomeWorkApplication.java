78013318bd9c5a35eafb1510bda82df0c50b07db
Icedragal <icedragal@gmail.com>
2022-12-27 12:05:55 +0300

    Initial commit

---
+++ src/main/java/me/zhuravlevegor/mavenhomework/MavenHomeWorkApplication.java
@@ -0,0 +1,13 @@
+package me.zhuravlevegor.mavenhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenHomeWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenHomeWorkApplication.class, args);
+    }
+
+}
